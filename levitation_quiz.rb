f1a12eb4e15c44ee92c6f98e8fda2ab7e010a9ac
Bill G <gbilly3@gmail.com>
2019-02-09 23:00:50 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,7 +1,10 @@
 
 def levitation_quiz
-	puts "Please type your name:"
-    name = gets
+	puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if 
+    answer == "stop"
+  end puts 
     puts "Your name is #{name}!"
 end
 
